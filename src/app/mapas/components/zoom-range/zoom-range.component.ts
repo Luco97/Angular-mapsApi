c87b31d0d5ebe5e0401af54bd71a5354a75f47c9
Luco <renatoluco@gmail.com>
2021-04-06 14:11:59 -0400

    Utilizando listeners para obtener datos del mapa (cantidad de Zoom, limite de zoom, latitud y longitud), tambien estamos utilizando el ngOnDestroy para destruir los listeners que todavia quedan (hay que destruirlos para evitar el uso excesivo de recursos)

--- src/app/mapas/components/zoom-range/zoom-range.component.ts
+++ src/app/mapas/components/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 
 @Component({
@@ -6,17 +6,54 @@ import * as mapboxgl from "mapbox-gl";
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
+  center: [number,number] = [-70.55695340172628,-33.50701202375152];
 
   constructor() { }
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', ()=>{});
+    this.mapa.off('zoom', ()=>{});
+    this.mapa.off('move', ()=>{});
+  }
 
-  ngOnInit(): void {
-    var map = new mapboxgl.Map({
-      container: 'mapa',
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container:  this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-70.55695340172628,-33.50701202375152],
-      zoom: 15
+      center: this.center,
+      zoom: this.zoomLevel
     });
+
+    this.mapa.on('zoom', (ev) =>{
+      this.zoomLevel = this.mapa.getZoom();
+    })
+
+    this.mapa.on('zoom', (ev) =>{
+      if( this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    })
+
+    this.mapa.on('move', (ev) =>{
+      this.center = [this.mapa.getCenter().lng,this.mapa.getCenter().lat]
+      //console.log(this.mapa.getCenter().lng,this.mapa.getCenter().lat);
+    })
+  }
+
+  zoomOut() {
+    this.mapa.zoomOut()
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn()
+  }
+
+  zoomCambio(valor: any){
+    this.mapa.zoomTo(Number(valor));
   }
 
 }
