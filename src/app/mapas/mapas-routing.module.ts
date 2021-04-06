b3827dfee2e057ba482f87c56f8290372d9ba4c0
Luco <renatoluco@gmail.com>
2021-04-06 11:24:02 -0400

    Creando componentes para la api

---
+++ src/app/mapas/mapas-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './components/full-screen/full-screen.component';
+import { MarcadoresComponent } from './components/marcadores/marcadores.component';
+import { ZoomRangeComponent } from './components/zoom-range/zoom-range.component';
+import { PropiedadesComponent } from './components/propiedades/propiedades.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path: 'fullscreen',
+        component: FullScreenComponent
+      },
+      {
+        path: 'marcadores',
+        component: MarcadoresComponent
+      },
+      {
+        path: 'zoom-range',
+        component: ZoomRangeComponent
+      },
+      {
+        path: 'propiedades',
+        component: PropiedadesComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'fullscreen'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MapasRoutingModule { }
