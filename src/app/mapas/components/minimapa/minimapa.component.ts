9255889d942eed26a9492736848e7056929cc497
Luco <renatoluco@gmail.com>
2021-04-06 18:46:14 -0400

    Creando propieddes, usando decorador Input para el componente minimap

--- src/app/mapas/components/minimapa/minimapa.component.ts
+++ src/app/mapas/components/minimapa/minimapa.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-minimapa',
   templateUrl: './minimapa.component.html',
   styleUrls: ['./minimapa.component.css']
 })
-export class MinimapaComponent implements OnInit {
+export class MinimapaComponent implements AfterViewInit {
 
+  @Input('coordenadas') lngLat: [number,number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef;
   constructor() { }
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive:false
+    });
 
-  ngOnInit(): void {
+    new mapboxgl.Marker()
+        .setLngLat(this.lngLat)
+        .addTo(mapa)
   }
+
+
 
 }
